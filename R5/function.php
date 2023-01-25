1a46746bcc1a42a737668638eb5be8bf6a811c0e
bismark <ansahbismark40@gmail.com>
2023-01-25 18:48:13 +0800

    pull

--- R5/function.php
+++ R5/function.php
@@ -1,3 +1,10 @@
 <?php
 
-function 
+function getTime($start , $end, $generateMin)
+{
+    $interval = $end - $start;
+    $trueEnd = intdiv($interval, $generateMin)*$generateMin;
+
+}
+
+print_r(getTime(100, 600, 90));
