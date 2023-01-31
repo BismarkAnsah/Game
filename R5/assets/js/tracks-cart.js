0b6f751344ed957712a5d505c589d4294fe88ab9
Maxwell <nanayawfixing@gmail.com>
2023-01-31 15:52:25 +0000

    latest trck fix

--- R5/assets/js/tracks-cart.js
+++ R5/assets/js/tracks-cart.js
@@ -2,111 +2,72 @@ console.log("hello world");
 
 // console.log(timePassed);
 
+const decrementButton = document.getElementById("decrement");
+const incrementButton = document.getElementById("increment");
+const result = document.getElementById("btnMultiBetCount");
+let currentValue = 1;
 
-    const decrementButton = document.getElementById("decrement");
-  const incrementButton = document.getElementById("increment");
-  const result = document.getElementById("btnMultiBetCount");
-  let currentValue = 1;
+decrementButton.addEventListener("click", function () {
+  if (currentValue > 1) {
+    currentValue -= 1;
+    result.value = "X" + currentValue;
+  }
+});
 
-  decrementButton.addEventListener("click", function() {
-    if (currentValue > 1) {
-      currentValue -= 1;
-      result.value = 'X' + currentValue;
-    }
-  });
+incrementButton.addEventListener("click", function () {
+  currentValue += 1;
+  result.value = "X" + currentValue;
+});
 
-  incrementButton.addEventListener("click", function() {
-    currentValue += 1;
-    result.value = 'X' + currentValue;
-  });
+const button = document.querySelector(".btn-mask-balance");
+const mask_balance = document.querySelector(".mask-balance");
+const unmask_balance = document.querySelector(".unmasked-balance");
+let isMasked = false;
 
+button.addEventListener("click", function () {
+  if (isMasked) {
+    mask_balance.classList.toggle("visually-hidden");
+    unmask_balance.classList.toggle("visually-hidden");
+    button.className = "bi bi-eye";
+    isMasked = false;
+  } else {
+    button.className = "bi bi-eye-slash";
+    mask_balance.classList.toggle("visually-hidden");
+    unmask_balance.classList.toggle("visually-hidden");
+    isMasked = true;
+  }
+});
 
-  const button = document.querySelector(".btn-mask-balance");
-  const mask_balance = document.querySelector(".mask-balance");
-  const unmask_balance = document.querySelector(".unmasked-balance");
-  let isMasked = false;
+const master = document.getElementById("master");
+const slaves = document.querySelectorAll(".slave");
 
-  button.addEventListener("click", function() {
-    if (isMasked) {
-        mask_balance.classList.toggle('visually-hidden')
-        unmask_balance.classList.toggle('visually-hidden');
-        button.className = 'bi bi-eye'
-        isMasked = false;
-    } else {
-        button.className = 'bi bi-eye-slash'
-        mask_balance.classList.toggle('visually-hidden')
-        unmask_balance.classList.toggle('visually-hidden');
-      isMasked = true;
-    }
-  });
+// console.log(master);
+master.addEventListener("change", function () {
+  for (let slave of slaves) {
+    slave.checked = master.checked;
+  }
+});
 
+const showModal = (title, button_type) => {
+  // showModal()
+  var myModal = new bootstrap.Modal(document.getElementById("m-betModal"));
+  document.querySelector(".modal-title").innerText = title;
+  document.querySelector(".button-type").innertext = button_type;
+  // myModal.closest(".modal-title").innerHTML = "title.innerHTML";
+  console.log(myModal);
 
-  const master = document.getElementById("master");
-  const slaves = document.querySelectorAll(".slave");
+  myModal.show();
+};
 
-  console.log(master)
-  master.addEventListener("change", function () {
-    for (let slave of slaves) {
-      slave.checked = master.checked;
-    }
-  });
-
-// $(".guestCounter li").on("click", function (element) {
-//     let operationType = $(this).attr("data-btn-type");
-//     // let oldValue = $(this).parent().find("input").val();
-//     console.log(operationType);
-//     let result = document.getElementById("result")
-//     let results = document.getElementById("result").value.replace('X', '');
-    
-//     let currentValue=1;
-//     // let newVal;
-//     if (operationType == "increment") {
-//         result = Number(results ++);
-//         console.log(results);
-//         result.value = 'X' + results;
-//     } 
-//     else {
-
-//         // Don't allow decrementing below zero
-
-//         if (currentValue > 1) {
-//             currentValue --;
-//             result.value = 'X' + currentValue;
-//             console.log(currentValue);
-
-//           }
-//     }
-//     console.log(results);
-//     console.log(currentValue);
-
-//     $(this).parent().find("input").val(result.value);
+const showCartArea = (id) => {
+  let triggerEl = document.getElementById(id)
+  let tabTrigger = new bootstrap.Tab(triggerEl)
+  
+  bootstrap.Tab.getInstance(triggerEl).show() // Select tab by name
+  $('html, body').animate({
+    scrollTop: $(".cart-track-bet").offset().top
+  }, 500);
+};
 
 
-
-    
-
-// //   decrementButton.addEventListener("click", function() {
-    
-// //   });
-
-// //   incrementButton.addEventListener("click", function() {
-    
-// //   });
-
-// // let newVal;
-// //     if (operationType == "increment") {
-// //          newVal = `${parseFloat(oldValue) + 1}x`;
-// //     } else {
-
-// //         // Don't allow decrementing below zero
-
-// //         let oldValues = oldValue.replace('x', '')
-
-// //         if (oldValues > 1) {
-// //              newVal = `${ parseFloat(oldValue) - 1}x`;
-// //         } else {
-// //             newVal = "1x";
-// //         }
-// //     }
-// //     $(this).parent().find("input").val(newVal);
-// });
+  
