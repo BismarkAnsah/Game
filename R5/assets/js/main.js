0f2e5944dd6a470014f8fd6e421783afd84dc938
BismarkAnsah <57173824+BismarkAnsah@users.noreply.github.com>
2023-02-15 23:20:24 +0000

    maintained current

--- R5/assets/js/main.js
+++ R5/assets/js/main.js
@@ -5,7 +5,6 @@
 // })
 
 const tooltipTriggerList = document.querySelectorAll(
-<<<<<<< HEAD
   '[data-bs-toggle="tooltip"]'
 );
 const tooltipList = [...tooltipTriggerList].map(
@@ -36,35 +35,3 @@ export function checkRemainingSelectOptions(attr) {
 //     $('select[name="first_draw"]').append(
 //     `<option value="${trackJson[nextIndex].trackId}">${trackJson[nextIndex].trackId}</option>`
 // );
-=======
-    '[data-bs-toggle="tooltip"]'
-  );
-  const tooltipList = [...tooltipTriggerList].map(
-    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
-  );
-  
-  
-  /**
-   * @param input string to tuncate
-   * @param truncateAfter tuncate after character at
-   */
-  export function truncateEllipsis(input, truncateAfter) {
-    if (input.length > truncateAfter) {
-      return input.substring(0, truncateAfter) + "...";
-    }
-    return input;
-  }
-  
-  /** returns the count of remaining options in a select element right from the current selected option*/
-  export function checkRemainingSelectOptions(attr) {
-    let selectedIndex = $(attr).prop("selectedIndex");
-    let count = $(`${attr} option:gt(" ${selectedIndex} "), option:eq(" ${selectedIndex} ")`).length;
-    console.log(count);
-    return count;
-  }
-  
-  // selectTrackIds.push(trackJson[nextIndex].trackId);
-  //     $('select[name="first_draw"]').append(
-  //     `<option value="${trackJson[nextIndex].trackId}">${trackJson[nextIndex].trackId}</option>`
-  // );
->>>>>>> Jo-Branch
