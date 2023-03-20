3308d950f6a7e466efe171b4da96bb6740d85a40
BismarkAnsah <57173824+BismarkAnsah@users.noreply.github.com>
2023-03-20 22:27:10 +0000

    updated starter.php

--- R5/add-ons/starter.php
+++ R5/add-ons/starter.php
@@ -1,36 +1,14 @@
 <body>
     <script>
-        const timeInterval = 58000;
-
-        function ajax(url, callback = null, method = 'GET', data = null) {
-            const xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        callback(null, xhr.responseText);
-                    } else {
-                        callback(new Error('Request failed with status ' + xhr.status), null);
-                    }
-                }
-            };
-            xhr.open(method, url, true);
-
-            if (method === 'POST') {
-                xhr.setRequestHeader('Content-Type', 'application/json');
-            }
-
-            xhr.send(JSON.stringify(data));
-        }
-
         function makeRequest() {
-            ajax('cron.php', function(err, response) {
+            fetch('cron.php').then(async response=>{
+                response = await response.json();
                 console.log(response);
-                let responseObj = JSON.parse(response);
-                let nextRequestTime = responseObj.nextRequestTime? responseObj.nextRequestTime-2: 2;
+                let nextRequestTime = response.nextRequestTime? response.nextRequestTime-2: 2;
+                console.log(nextRequestTime)
                 setTimeout(makeRequest, (nextRequestTime)*1000);
-            });
+            })
         }
-
         makeRequest();
     </script>
 </body>
